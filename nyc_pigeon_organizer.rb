9d7233ea57c929496b10cb8b4c42340e0453ff8b
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 04:12:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
+def loop_and_add(data, name)
+  new_array = []
+  key_array = data.keys
+  key_array.each do |key|
+    new_array << 
+  end
+  new_array
+end
+
+def add_data_to_name(old_data, new_data, name)
+  new_hash = new_data
+  
+  new_hash[name][:color] = loop_and_add(old_data[:color], name)
+  new_hash[name][:gender] = loop_and_add(old_data[:gender], name)
+  new_hash[name][:lives] = loop_and_add(old_data[:lives], name)
+  
+  new_hash
+end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pidgeon_names = {}
+  data[:gender][:male].each do |name|
+    pidgeon_names << {
+      name => {}
+    }
+  end
+  data[:gender][:female].each do |name|
+    pidgeon_names << {
+      name => {}
+    }
+  end
+  
+  name_keys = pidgeon_names.keys
+  name_keys.each do |name|
+    pidgeon_names[name] = add_data_to_name(data, pidgeon_names, name)
+  end
+  
+  
+  
+  
+  pidgeon_names
 end
